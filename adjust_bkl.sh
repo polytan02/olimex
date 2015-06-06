ee5a7a763aebd874ed133408e170809ab2c1897c
polytan02 <polytan02@mcgva.org>
2015-06-06 12:22:28 +0100

    Ajout de couleurs parce que c'est plus joli !

--- adjust_bkl.sh
+++ adjust_bkl.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
+# Setup of colours for error codes
+set -e
+
+txtgrn=`tput setaf 2`;
+txtred=`tput setaf 1`;
+txtcyn=`tput setaf 6`;
+txtpur=`tput setaf 5`;
+txtrst=`tput sgr0`;
+
+failed=[${txtred}ERREUR${txtrst}];
+ok=[${txtgrn}OK${txtrst}];
+info=[${txtcyn}INFO${txtrst}];
+script=[${txtpur}SCRIPT${txtrst}];
+warning=[${txtred}ATTENTION${txtrst}];
+
 # Make sure only root can run our script
 if [[ $EUID -ne 0 ]];
-        then   echo "Besoin des droits root";
+        then   echo -e "$failed Besoin des droits root";
         exit;
 fi;
 
-read -e -p "Ajuster luminosité (1=off et 98=max) : " changebkl;
+echo -e ""; read -e -p "$script Ajuster luminosité (1=off et 98=max) : " changebkl;
 
 if [[ "$changebkl" =~ ^[0-9]+$ ]] && [ "$changebkl" -ge 1 -a "$changebkl" -le 98 ] ;
 	then echo $changebkl > /sys/class/pwm-sunxi/pwm0/duty_percent;
-#	echo $changebkl;
-	else echo -e "\n ERREUR ! la valeur doit être un entier compris entre 1 (off) et 98 (max)\n ";
+	echo -e "\n$ok Luminosité ajustée \n";
+	else echo -e "\n$failed ! la valeur doit être un entier compris entre 1 (off) et 98 (max)\n";
 fi;
 
 
